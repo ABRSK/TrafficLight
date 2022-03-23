9ed0a39c778105051c2a6cdd59e6cdf2756c058e
AreyBRSK <areybrsk@gmail.com>
2022-03-23 12:57:49 +0300

    Working version

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,20 +13,47 @@ class ViewController: UIViewController {
     @IBOutlet var greenLight: UIView!
     @IBOutlet var controlButton: UIButton!
     
+    let turnedOnAlpha = 1.0
+    let turnedOffAlpha = 0.25
+    var currentColor = "none"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLight.alpha = 0.25
-        yellowLight.alpha = 0.25
-        greenLight.alpha = 0.25
-        
-        redLight.layer.cornerRadius = redLight.frame.width / 2
-        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
-        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        redLight.alpha = turnedOffAlpha
+        yellowLight.alpha = turnedOffAlpha
+        greenLight.alpha = turnedOffAlpha
         
         controlButton.layer.cornerRadius = 10
     }
+    
+    override func viewDidLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
 
+    @IBAction func switchColor() {
+        switch currentColor {
+        case "red":
+            currentColor = "yellow"
+            redLight.alpha = turnedOffAlpha
+            yellowLight.alpha = turnedOnAlpha
+        case "yellow":
+            currentColor = "green"
+            yellowLight.alpha = turnedOffAlpha
+            greenLight.alpha = turnedOnAlpha
+        case "green":
+            currentColor = "red"
+            greenLight.alpha = turnedOffAlpha
+            redLight.alpha = turnedOnAlpha
+        default:
+            currentColor = "red"
+            redLight.alpha = turnedOnAlpha
+            controlButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 
 }
 
