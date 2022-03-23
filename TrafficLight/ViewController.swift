04373b509ec0aa8700ddc444a6a8b3b963569fc0
AreyBRSK <areybrsk@gmail.com>
2022-03-23 11:58:30 +0300

    Some visual properties added

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var controlButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = 0.25
+        yellowLight.alpha = 0.25
+        greenLight.alpha = 0.25
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        controlButton.layer.cornerRadius = 10
     }
 
 
